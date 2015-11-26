30c823c155d67a638566780c6edccff140f820f6
Adrián <ablanco@ELCONFI035.elconfidencial.int>
2015-11-26 12:02:20 +0100

    create_eg15

---
+++ README.md
@@ -0,0 +1,14 @@
+# Últimos artículos publicados sobre los candidaros a las elecciones generales en España
+
+- Herramienta basada en el desarrollo de Jason lee French
+- Este agreagador de noticias es posible gracias a BBC's Juicer API
+- Tutorial seguido en abril en el meetup Journocoders London
+
+[Tutorial on BBC's Juicer API](https://github.com/basilesimon/using-an-api-tutorial/blob/master/tutorial.md)
+
+Todos
+------
+
+- Actualizar con Alberto Garzón
+- Repo de artículos pasados
+- Añadir var euros
---
+++ js/init.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  //Haha Clegg you're last. (other leader positions may not reflect the political beliefs of the author)
+  var leaders  = ['Mariano%20Rajoy','Pedro%20Sanchez','Albert%20Rivera','Pablo%20Iglesias'],
+      apikey   = 'YB0MY3VMHyllzPqEf5alVj5bUvGpvDVi',
+      queryURL = "http://data.test.bbc.co.uk/bbcrd-juicer/articles?apikey=" + apikey + "&recent_first=true&q=";
+
+  function getResults(query,leader) {
+    $('.container').append("<div class='" + prettyStr(leader) + "'></div>");
+    $.getJSON(query, function(data) {
+      var items = [];
+      $.each(data.hits, function(key, val){
+        // Strange. Some entries don't have titles/content. We just want the ones that do!
+        if (val.title) {
+          // Super-long html is icky.
+          // TODO - make better somehow.
+          var htmlString = [
+                            "<div class='panel panel-default'><div class='panel-heading'><h3 class='panel-title'><a target='_blank' href='",
+                            val.url,
+                            "'>",
+                            val.title,
+                            "</a> (source: ",
+                            val.source['source-name'],
+                            ")</h3></div><div class='panel-body'>",
+                            val.description,
+                            "</div></div>"
+                          ].join('');
+          items.push(htmlString);
+        }
+      });
+      $('.' + prettyStr(leader)).append(items.join(""));
+    });
+  }
+
+  function prettyStr(str) {
+    return str.replace('%20','-').toLowerCase();
+  }
+
+  for (var i = 0; i < leaders.length; i++){
+    getResults(queryURL + leaders[i], leaders[i]);
+  }
+
+});
